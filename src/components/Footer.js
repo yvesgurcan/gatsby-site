95961add66ea554ad3109465b8c9354164d63800
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-10-18 17:20:27 -0700

    linting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,17 @@
 import React from "react"
-import styled from 'styled-components';
+import styled from "styled-components"
 
-import Title from './Title';
-import Container from './Container';
+import Title from "./Title"
+import Container from "./Container"
 
 const Footer = () => (
   <FooterContainer>
     <Title />
   </FooterContainer>
-);
-
-
+)
 
 const FooterContainer = styled(Container)`
   justify-content: center;
-`;
+`
 
-export default Footer;
+export default Footer
