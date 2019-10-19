b33f071fbec818b59ff88aa04d0c70b36537b8af
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-10-18 17:20:48 -0700

    add custom font

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+function SEO({ description, lang, meta, title, link = [] }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -67,15 +67,21 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
+      link={[
+        {
+          href: "https://fonts.googleapis.com/css?family=Montserrat:400,600",
+          rel: "stylesheet",
+        },
+      ].concat(link)}
     />
   )
 }
 
 SEO.defaultProps = {
-  lang: 'en',
+  lang: "en",
   meta: [],
-  description: '',
-  title: '',
+  description: "",
+  title: "",
 }
 
 SEO.propTypes = {
