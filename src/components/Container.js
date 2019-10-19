0dc9d055bbd597485fc53b9e28d3c66807ef5c9a
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-10-18 17:20:39 -0700

    fix styles

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,16 +1,16 @@
-import styled from 'styled-components';
+import styled from "styled-components"
 
 const Container = styled.div`
   display: flex;
-  width: 98vw;
+  width: 100vw;
   background: black;
   margin: 0;
   padding: 5px;
-  padding-right: 40px;
 
-  &, a {
+  &,
+  a {
     color: white;
   }
-`;
+`
 
-export default Container;
+export default Container
